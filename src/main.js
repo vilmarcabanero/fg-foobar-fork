05bd18ab5b99a47a4fd164a39f168e55d4e6eba9
Mark Anthony Vivar <markanthonyvivar24@gmil.com>
2023-08-12 21:51:01 +0800

    fixing

--- src/main.js
+++ src/main.js
@@ -8,63 +8,11 @@ import { createApp } from 'vue'
 import router from "./router";
 import App from './App.vue'
 
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { library } from "@fortawesome/fontawesome-svg-core";
 import AOS from 'aos';
 import  VueTypewriterEffect  from "vue-typewriter-effect";
 import VideoPlayer from '@videojs-player/vue'
 
-import { 
-  faSearch, 
-  faPhone, 
-  faPhoneVolume,
-  faLocationDot,
-  faCircleCheck,
-  faCaretRight,
-  faArrowRight,
-  faCaretDown,
-  faCaretUp,
-  faQuoteLeft,
-  faQuoteRight,
-  faEnvelope,
-  faChevronRight,
-  faStar,
-  faCheck,
-  faBars,
-  faXmark
-} from "@fortawesome/free-solid-svg-icons";
-import { 
-  faSquareFacebook, 
-  faSquareYoutube, 
-  faSquareInstagram, 
-  faLinkedin,
-  faTiktok,
- } from '@fortawesome/free-brands-svg-icons';
 
-library.add(
-  faSearch, 
-  faPhone, 
-  faPhoneVolume,
-  faLocationDot,
-  faCircleCheck,
-  faCaretRight,
-  faArrowRight,
-  faCaretDown,
-  faCaretUp,
-  faQuoteLeft,
-  faQuoteRight,
-  faEnvelope,
-  faChevronRight,
-  faStar,
-  faCheck,
-  faBars,
-  faXmark,
-  faSquareFacebook, 
-  faSquareYoutube, 
-  faSquareInstagram,  
-  faLinkedin,
-  faTiktok,
-)
 
 AOS.init({
   once: true,
@@ -75,8 +23,6 @@ const app = createApp(App);
 app.use(router);
 app.use(VideoPlayer);
 
-
-app.component("FontAwesomeIcon", FontAwesomeIcon);
 app.component("vue-typewriter-effect", VueTypewriterEffect);
 
 app.mount('#app');
